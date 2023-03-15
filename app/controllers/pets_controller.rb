19a1ef8e69e8d8754fb300ccb7d405a62de69126
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-15 18:01:05 +0300

    Done

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,5 @@
+class PetsController < ApplicationController
+    def index
+        # {hello:"hello pets"}.to_json()
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'pets'  ,to: 'pets#index'
+
+  resource :pets, only: [:show,:create,:new,:edit,:update ]
 end
