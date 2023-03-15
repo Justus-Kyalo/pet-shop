b32c647ec9423ce1ad991cbf869c1bfdfd51a7f3
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-15 16:33:34 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Pet.create(name:'Timmy' ,breed:'Siamese')
+# Pet.create(name:'Lucy' ,breed:'Tabby')
+# Pet.create(name:'King' ,breed:'Siberian')
+# Pet.create({name:'Timmy' ,breed:'Siamese'} ,{name:'Lucy' ,breed:'Tabby'} ,{name:'King' ,breed:'Siberian'})
+Pet.create(name:Faker::Creature::Cat.name ,breed:Faker::Creature::Cat.breed)
